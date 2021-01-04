c54f09b9c996f8c726030418dbd99d75a3679fbd
erenarkan04 <ahmeta04@gmail.com>
2021-01-04 05:33:45 -0500

    #19 variables

--- variables.java
+++ variables.java
@@ -1,10 +1,10 @@
 
-ls
-:
-
-asda
-:wq
+
+public class variables {
+	public static void main (String[] args) {
+		String myJob = "Financial analyst";  
+		System.out.println(myJob); 
+	} 
+}
 
 
-
-
