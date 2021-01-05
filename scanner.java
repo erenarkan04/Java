5a028fe66c62483f57bfa6b2c89f1c721dacedbd
erenarkan04 <ahmeta04@gmail.com>
2021-01-05 02:53:43 -0500

    added scanner exercise

---
+++ scanner.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class scanner {
+	public static void main (String[] args) {
+
+	Scanner stringInput = new Scanner(System.in); 
+	Scanner intInput = new Scanner(System.in); 
+	Scanner doubleInput = new Scanner(System.in); 
+	Scanner booleanInput = new Scanner(System.in); 
+
+	
+
+
+
+
+	//System.out.println(stringInput.next()); 
+ 
+	System.out.println("Enter your name: "); 
+	System.out.println("Your name is: " + stringInput.next()); 
+
+
+
+}
+
+}
+
+
+
+
